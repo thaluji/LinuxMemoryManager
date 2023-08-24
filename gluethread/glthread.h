4371dc037addee406bf306f89cd3101098163516
Muhammed Thalzih A P <83165622+thaluji@users.noreply.github.com>
2023-08-25 03:49:54 +0530

    Update glthread.h

--- gluethread/glthread.h
+++ gluethread/glthread.h
@@ -1,34 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  glthread.h
- *
- *    Description:  This file defines the Data structure and APIs for Glue thread
- *
- *        Version:  1.0
- *        Created:  Monday 12 March 2018 02:01:51  IST
- *       Revision:  1.0
- *       Compiler:  gcc
- *
- *         Author:  Er. Abhishek Sagar, Networking Developer (AS), sachinites@gmail.com
- *        Company:  Brocade Communications(Jul 2012- Mar 2016), Current : Juniper Networks(Apr 2017 - Present)
- *        
- *        This file is part of the SPFComputation distribution (https://github.com/sachinites).
- *        Copyright (c) 2017 Abhishek Sagar.
- *        This program is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU General Public License as published by  
- *        the Free Software Foundation, version 3.
- *
- *        This program is distributed in the hope that it will be useful, but 
- *        WITHOUT ANY WARRANTY; without even the implied warranty of 
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- *        General Public License for more details.
- *
- *        You should have received a copy of the GNU General Public License 
- *        along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * =====================================================================================
- */
 
 #ifndef __GLUETHREAD__
 #define __GLUETHREAD__
